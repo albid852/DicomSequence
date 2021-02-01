374d7e33b997dfb9c2feb48b79b61f66c703e9ed
albid852 <66276000+albid852@users.noreply.github.com>
2021-01-31 22:19:25 -0500

    Add files via upload

---
+++ PlotDCM.py
@@ -0,0 +1,98 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from preprocessing import apply_clahe, apply_cr_normalization, apply_fiji_normalization
+from typing import Union
+
+
+# SLICE VIEWER
+def multi_slice_viewer(volume: np.ndarray):
+    """
+    Go slice-by-slice through a sequence of images stacked as a volume. Recommended to
+    use for viewing the output of DcmSequence.interpolate_volume()
+    :param volume: Stack of images
+    :return: None
+    """
+    def process_key(event):
+        f = event.canvas.figure
+        a = f.axes[0]
+        if event.key == 'left':
+            previous_slice(a)
+        elif event.key == 'right':
+            next_slice(a)
+        a.set_title(str(a.index))
+        f.canvas.draw()
+
+    def previous_slice(a):
+        vol = a.volume
+        a.index = (a.index - 1) % vol.shape[0]  # wrap around using %
+        a.images[0].set_array(vol[a.index])
+
+    def next_slice(a):
+        vol = a.volume
+        a.index = (a.index + 1) % vol.shape[0]
+        a.images[0].set_array(vol[a.index])
+
+    fig, ax = plt.subplots()
+    ax.volume = volume
+    ax.index = 0
+    ax.set_title(str(ax.index))
+    ax.imshow(volume[ax.index], cmap='gray')
+    print("Use the right arrow key to go to the next slice")
+    print("Use the left arrow key to go to the previous slice")
+    fig.canvas.mpl_connect('key_press_event', process_key)
+
+
+# PLOTTING PROCESSING COMPARISONS
+def plot_comparisons(original: np.ndarray, cr: Union[np.ndarray, None] = None,
+                     fiji: Union[np.ndarray, None] = None, clahe: Union[np.ndarray, None] = None,
+                     name: str = "UNNAMED"):
+    """
+    Visualization of the different image processing algorithms in a 2x2 grid using
+    matplotlib.
+    :param original: original image
+    :param cr: cr processed version of the image
+    :param fiji: fiji processed version of the image
+    :param clahe: clahe processed version of the image
+    :param name: Name of the image. Default is UNNAMED
+    :return: plotted figure
+    """
+    fig = plt.figure()
+    plt.axis('off')
+    plt.tick_params(axis='both')
+    ax1 = fig.add_subplot(221)
+    ax2 = fig.add_subplot(222)
+    ax3 = fig.add_subplot(223)
+    ax4 = fig.add_subplot(224)
+
+    fig.suptitle(name, fontsize=16)
+    ax1.title.set_text('Original')
+    ax2.title.set_text('CR')
+    ax3.title.set_text('FIJI')
+    ax4.title.set_text('CLAHE')
+
+    ax1.get_xaxis().set_visible(False)
+    ax1.get_yaxis().set_visible(False)
+
+    ax2.get_xaxis().set_visible(False)
+    ax2.get_yaxis().set_visible(False)
+
+    ax3.get_xaxis().set_visible(False)
+    ax3.get_yaxis().set_visible(False)
+
+    ax4.get_xaxis().set_visible(False)
+    ax4.get_yaxis().set_visible(False)
+
+    if cr is None:
+        cr = apply_cr_normalization(original)
+    if clahe is None:
+        clahe = apply_clahe(original)
+    if fiji is None:
+        fiji = apply_fiji_normalization(original)
+
+    ax1.imshow(original, cmap='gray')
+    ax2.imshow(cr, cmap='gray')
+    ax3.imshow(fiji, cmap='gray')
+    ax4.imshow(clahe, cmap='gray')
+
+    plt.show()
+    return fig
---
+++ preprocessing.py
@@ -0,0 +1,145 @@
+import numpy as np
+import pydicom
+import cv2
+from scipy import interpolate
+# from typing import Tuple, List
+
+# CONVERT INT TO UINT
+def convert_int_to_uint(img: np.ndarray):
+    """
+    Conversion of int16 to uint16
+    :param img: numpy array to convert
+    :return: numpy array as type uint16
+    """
+    if img.dtype == np.int16:
+        img_min = np.min(img)
+        img += abs(img_min)
+        return img.astype(np.uint16)
+
+
+# PREPROCESSING IMAGES
+def apply_clahe(img: np.ndarray, clip_lim: int = 40, tile_grid_size=(8, 8)):
+    """
+    Applies CV2's clahe algorithm to an image array.
+    :param img: Image to apply clahe to
+    :param clip_lim: All bins in the color histogram above the limit are clipped
+    and distributed to the existing bins.
+    :param tile_grid_size: tile shape
+    :return: Clahe image as a numpy array. Still retains whatever dtype the img started with
+    """
+    clahe = cv2.createCLAHE(clipLimit=clip_lim, tileGridSize=tile_grid_size)
+    img = convert_int_to_uint(img)
+    clahe_img = clahe.apply(img)
+    return clahe_img
+
+
+def apply_fiji_normalization(img: np.ndarray):
+    """
+    Applies a fiji normalization to reduce the given image to a 255 range. Looks exactly
+    the same as the original image.
+    :param img: Image to normalize
+    :return: Normalized image as an 8-bit numpy array.
+    """
+    img_min, img_max = int(np.min(img)), int(np.max(img))
+    scale = 256. / (img_max - img_min + 1)
+    x = img & 0xffff
+    x -= img_min
+    x[x < 0] = 0
+    x = x * scale + 0.5
+    x[x > 255] = 0
+    return x.astype(np.uint8)
+
+
+def apply_cr_normalization(img: np.ndarray):
+    """
+    Applies the following normalization to reduce the image to a 0-1 range:
+    img / (abs(image mean) + 3 * (image standard deviation))
+    Then multiplies by 255 and clips the image between 0-255.
+    :param img:
+    :return: Normalized image as an 8-bit numpy array.
+    """
+    mu = np.mean(img)
+    sigma = np.std(img)
+    tmp = img / (abs(mu) + 3 * sigma)
+    tmp *= 255
+    uint8_img = np.clip(tmp, 0, 255).astype(np.uint8)
+    return uint8_img
+
+
+# GET PNG LIST FROM DICOMS
+def get_png(dcm_list, clahe: bool = False, norm_alg: int = 1):
+    """
+    Get list of png images and list of file names by converting the current dicoms in the
+    collection to 8-bit using the preferred norm-alg.
+    :param dcm_list: list of dicoms to turn to png
+    :param clahe: whether or not to perform clahe on the images
+    :param norm_alg: which normalization algorithm to use to get the image between 0-255.
+    If using clahe, recommended to set norm_alg = 0. norm_alg = 1 is for the fiji
+    normalization. norm_alg = 2 is for CR normalization.
+    :return: List of file names (not the path), list of png images
+    """
+    if norm_alg < 0 or norm_alg > 2:
+        raise ValueError("norm_alg must be between 0 and 2")
+
+    images = []
+
+    for ds in dcm_list:
+
+        if ds.pixel_array.dtype == np.uint8:
+            images.append(ds.pixel_array)
+
+        elif ds.pixel_array.dtype == np.uint16 \
+                or ds.pixel_array.dtype == np.int16:
+            image = convert_int_to_uint(ds.pixel_array)
+            if clahe:
+                clip_lim = 40
+                tile_grid_size = (8, 8)
+                image = apply_clahe(image, clip_lim, tile_grid_size)
+
+            if norm_alg == 0:
+                image = np.uint8((image / np.max(image)) * 255)
+            elif norm_alg == 1:
+                image = apply_fiji_normalization(image)
+            elif norm_alg == 2:
+                image = apply_cr_normalization(image)
+
+            images.append(image)
+
+    return images
+
+
+# GET INTERPOLATED IMAGES FROM NUMPY ARRAYS
+def interpolate_volume(volume: np.ndarray, num_slices: int = 4):
+    """
+    Create an interpolated volume from the image stack. This will interpolate slices of
+    images between every consecutive pair of slices. The num_slices determines how
+    many interpolated slices are between the original slices and the separation between them.
+    :param volume: array of images to interpolate between
+    :param num_slices: Number of interpolated slices between the original slices
+    :return: the entire interpolated volume
+    """
+    if len(volume.shape) != 3:
+        raise ValueError("volume must be of shape (depth, height, width)")
+    depth, img_width, img_height = volume.shape
+    # set up interpolator
+    points = (np.arange(depth), np.arange(img_height), np.arange(img_width))  # (z, y, x)
+    rgi = interpolate.RegularGridInterpolator(points, volume)
+
+    # get slices with separation of 1/(num_slices + 1)
+    g = np.mgrid[1:num_slices + 1, :img_height, :img_width]
+    coords = np.vstack(map(np.ravel, g)).transpose().astype(np.float16)
+    coords[:, 0] *= 1 / (num_slices + 1)
+
+    stack = np.zeros((depth + num_slices * (depth - 1), img_height, img_width), dtype=np.uint8)
+
+    # visualize whole volume as slices
+    for n in range(depth):
+        stack[n * (num_slices + 1)] = volume[n]
+        print("SLICE NUMBER:", n + 1)
+        if n < depth - 1:
+            interp_slices = rgi(coords).reshape((num_slices, img_height, img_width)).astype(np.uint8)
+            for i in range(num_slices):
+                print("\t", i + 1)
+                stack[n * (num_slices + 1) + i + 1] = interp_slices[i]
+            coords[:, 0] += 1
+    return stack
