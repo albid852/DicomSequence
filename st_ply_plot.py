fffb69c8f97114cb70e9c832aac3f5e940e38667
albid852 <66276000+albid852@users.noreply.github.com>
2021-03-05 18:22:30 -0500

    Update st_ply_plot.py

--- st_ply_plot.py
+++ st_ply_plot.py
@@ -1,6 +1,7 @@
 # import streamlit as st
 import numpy as np
 from scipy import interpolate
+from scipy.ndimage import zoom
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 import meshio
@@ -19,22 +20,26 @@ def count_nonzero(vol: np.ndarray) -> int:
     return c
 
 # try just appending to rgb and points (all in one go) instead of counting then indexing
-def vol2points(vol: np.ndarray) -> np.ndarray:
+def vol2points(vol: np.ndarray, min_val: int = 0) -> np.ndarray:
     cpixels = count_nonzero(vol)
+    print(f"Number of non-black pixels: {cpixels}")
     rgb = np.zeros((cpixels, 3), dtype=np.uint8)
     points = np.zeros((cpixels, 3), dtype=np.int16)
     n = 0
     for i in range(vol.shape[0]):  # z
         for j in range(vol.shape[1]):  # y
             for k in range(vol.shape[2]):  # x
-                if vol[i, j, k].all() != 0:
+                if n % 50000 == 0:
+                    print(f"{n} points written")
+                if vol[i, j, k] > min_val:
                     points[n] = [k, j, i]
                     rgb[n] = vol[i, j, k, :3]
                     n += 1
+    print(f"{n} points written")
     return rgb, points
 
 
-def points2vol(points: np.ndarray, rgb: np.ndarray, yx_shape=(256, 256)) -> np.ndarray:
+def points2vol(points: np.ndarray, rgb: np.ndarray) -> np.ndarray:
     x_max = np.max(points[:, 0])
     y_max = np.max(points[:, 1])
     points[:, 2] += abs(np.min(points[:, 2]))  # making sure z are positive
@@ -61,18 +66,27 @@ rgb = np.array(list(mesh.point_data.values())).T
 
 np.unique(xyz[:, 2])
 
-vol = points2vol(xyz, rgb, yx_shape=(256, 256))  # downsample and conversion works
+vol = points2vol(xyz, rgb)  # conversion works
 
-# NEXT is to convert from volume to points
-new_rgb, new_xyz = vol2points(vol)
+new_array = zoom(vol, (0.5, 0.5, 0.5, 1))  # downsample = zooming in and out
+# multi_slice_viewer(vol)
+# multi_slice_viewer(new_array)
 
-multi_slice_viewer(vol)
+vol_min = np.min(rgb[:, 0])  # assuming rgb are all the same (grayscale image)
+new_rgb, new_xyz = vol2points(vol, min_val=vol_min)
+new_rgb_resized, new_xyz_resized = vol2points(new_array, min_val=vol_min)
+
+# Now write a new mesh with the RESIZED one
 
 
-# fig = plt.figure()
-# ax = plt.axes(projection='3d')
-# ax.set_axis_off()
-# ax.scatter(xyz[:, 2], xyz[:, 1], xyz[:, 0], c=rgb[:]/255., s=0.1)
+
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+ax.set_axis_off()
+# Problem: Surface of mesh coloring is very messed up from the zoom. Probably can't fix it.
+ax.scatter(new_xyz_resized[:, 2], new_xyz_resized[:, 1], new_xyz_resized[:, 0], c=new_rgb_resized[:]/255., s=0.1)
+ax.scatter(new_xyz[:, 2], new_xyz[:, 1], new_xyz[:, 0], c=new_rgb[:]/255., s=0.1)
+
 # st.pyplot(fig)
 
 
